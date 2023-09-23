cfae2b7bf585b207dcdd8e842311ad07d4f2ef2c
THE-MDA <dmitrymilchakov@mail.ru>
2023-09-23 22:21:35 +0300

    Initial Commit

---
+++ project 1/project_1App.swift
@@ -0,0 +1,32 @@
+//
+//  project_1App.swift
+//  project 1
+//
+//  Created by Дмитрий Мильчаков on 23.09.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct project_1App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
